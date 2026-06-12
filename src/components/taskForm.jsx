04ec77050057e28272e9de93e8b973b77c9ccff2
Martina Gullon <martinagullon@hotmail.com>
2026-06-12 20:01:06 -0300

    Estilos con CSS Modules aplicados a Task Form y responsividad verificada

--- src/components/taskForm.jsx
+++ src/components/taskForm.jsx
@@ -1,30 +1,32 @@
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
-import Box from '@mui/material/Box'
 import useTaskForm from '../hooks/useTaskForm'
 import useTaskStore from '../store/taskStore'
+import styles from './taskForm.module.css'
 
 const TaskForm = () => {
   const addTask = useTaskStore((state) => state.addTask)
   const { title, handleChange, handleSubmit } = useTaskForm(addTask)
 
   return (
-    <Box
-      component="form"
-      onSubmit={handleSubmit}
-      sx={{ display: 'flex', gap: 2, marginBottom: 3 }}
-    >
+    <form onSubmit={handleSubmit} className={styles.form}>
       <TextField
         label="Nueva tarea"
         variant="outlined"
         value={title}
         onChange={handleChange}
         fullWidth
+        className={styles.input}
       />
-      <Button type="submit" variant="contained" color="primary">
+      <Button
+        type="submit"
+        variant="contained"
+        color="primary"
+        className={styles.button}
+      >
         Agregar
       </Button>
-    </Box>
+    </form>
   )
 }
 
